6e96993a8521ca6537f9fc82d240b586b6b2c73f
Julian-Marius-Fairfax <julianmariusfairfax@gmail.com>
2020-06-13 16:30:49 +0200

    Upload

---
+++ Catalog.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+y_installer_url="http://updates-http.cdn-apple.com/2019/cert/061-41343-20191023-02465f92-3ab5-4c92-bfe2-b725447a070d/InstallMacOSX.dmg"
+y_installer_name="Install OS X Yosemite"
+y_installer_version="10.10.5"
+
+
+ec_installer_url="http://updates-http.cdn-apple.com/2019/cert/061-41424-20191024-218af9ec-cf50-4516-9011-228c78eda3d2/InstallMacOSX.dmg"
+ec_installer_name="Install OS X El Capitan"
+ec_installer_version="10.11.6"
+
+
+s_installer_url="http://updates-http.cdn-apple.com/2019/cert/061-39476-20191023-48f365f4-0015-4c41-9f44-39d3d2aca067/InstallOS.dmg"
+s_installer_name="Install macOS Sierra"
+s_installer_version="10.12.6"
+
+
+hs_installer_url="06/50/041-91758-A_M8T44LH2AW/b5r4og05fhbgatve4agwy4kgkzv07mdid9"
+hs_installer_key="041-91758"
+hs_installer_name="Install macOS High Sierra"
+hs_installer_version="10.13.6"
+
+
+m_update_option="0"
+m_combo_update_option="0"
+
+m_installer_url="17/32/061-26589-A_8GJTCGY9PC/25fhcu905eta7wau7aoafu8rvdm7k1j4el"
+m_installer_key="061-26589"
+m_installer_name="Install macOS Mojave"
+m_installer_version="10.14.6"
+
+m_update_url="null"
+m_update_key="null"
+m_update_version="null"
+
+m_combo_update_url="null"
+m_combo_update_key="null"
+
+
+c_update_option="1"
+c_combo_update_option="1"
+
+c_installer_url="22/25/061-70033-A_0AY28AEJF9/am7836x4645ittkll95lsmfk166h9yeivs"
+c_installer_key="061-70033"
+c_installer_name="Install macOS Catalina"
+c_installer_version="10.15.5"
+
+c_update_url="36/17/001-13498-A_C03K14K54F/7xjym01csi4l4vlpxb9eux51gdqkc9rrsh"
+c_update_key="001-13498"
+c_update_version="10.15.6"
+
+c_combo_update_url="48/45/001-13499-A_0QXH5MM99G/9si8r0u7150dkdxwub57lnuu8dbdx8xmek"
+c_combo_update_key="001-13499"
---
+++ README.md
@@ -0,0 +1,18 @@
+<img src="https://github.com/rmc-team/macos-downloader/raw/master/Installer.png" width="256">
+
+# macOS Downloader
+macOS Downloader is a command line tool for downloading macOS installers and beta updates
+
+# Contributors
+I'd like to the thank the following people, and many others, for their research, help, and inspiration.
+- [pbzx](https://github.com/NiklasRosenstein/pbzx) compiled by [dosdude1](https://github.com/dosdude1)
+- [curl](https://curl.haxx.se/) compiled by [SiliconExarch](https://github.com/SiliconExarch)
+
+# Links
+- [RMC GitHub](https://github.com/rmc-team)
+- [RMC Website](https://www.rmc-team.ch)
+- [RMC Twitter](https://twitter.com/_rmcteam)
+
+# Usage
+chmod +x ./macOS\ Downloader.sh —> makes the script runnable  
+sudo ./macOS\ Downloader.sh —> runs the script with root permissions  
